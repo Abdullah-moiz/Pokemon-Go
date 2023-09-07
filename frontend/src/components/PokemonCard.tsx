1c902203f70fa03dd38a51e860a8958dbdc28f2e
Abdullah-moiz <mrmoiz.dev@gmail.com>
2023-09-07 06:59:57 +0500

    adding delete and update + fixing bugs

--- frontend/src/components/PokemonCard.tsx
+++ frontend/src/components/PokemonCard.tsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
 import { Pokemon } from "../types";
+import { AiFillDelete, AiFillEdit } from "react-icons/ai";
+import { motion } from "framer-motion";
+
 
 type PokemonCardProps = {
   key: string;
   card: Pokemon;
 };
 
-export default function PokemonCard({card , key} : PokemonCardProps) {
+export default function PokemonCard({ card, key }: PokemonCardProps) {
+  const [Hovered, setHovered] = useState(false);
+
+
+
+  const handleMouseEnter = () => {
+    setHovered(true);
+    
+  };
+
+  const handleMouseLeave = () => {
+    setHovered(false);
+    
+  };
+
+
   return (
-    <div key={key} className="w-72 hover:scale-105 transition-all duration-700 cursor-pointer h-[30rem] p-1 border border-black rounded m-2">
+    <motion.div
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      key={key} className="w-72 relative hover:scale-105 transition-all duration-700 cursor-pointer h-[30rem] p-1 border border-black rounded m-2">
       <div className="w-full h-72 rounded flex items-center  justify-center">
         <img src={card?.imageURL} alt="no img" className=" w-full h-full rounded object-fill" />
       </div>
@@ -32,7 +54,17 @@ export default function PokemonCard({card , key} : PokemonCardProps) {
           </div>
         </div>
       </div>
-    </div>
+      {Hovered && (
+        <motion.div
+          className="absolute bottom-0 left-0 w-full h-44 bg-slate-950/50 transition-all duration-500 flex items-center justify-center"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+        >
+          <AiFillDelete className="text-red-600 text-2xl m-2" />
+          <AiFillEdit className="text-green-600 text-2xl m-2" />
+        </motion.div>
+      )}
+    </motion.div >
 
   )
 }
